aa247aa4f09b423dee4b7e4f9077d82996ff65dc
matthcol <matthias.colin@gmail.com>
2025-11-19 10:28:15 +0100

    ddl for big db

---
+++ sql-init-bigdb/01-tables.sql
@@ -0,0 +1,72 @@
+
+create table person (
+	id serial constraint pk_person primary key,
+	name varchar(150) not null,
+	birthdate date null,
+	birth_year smallint null,
+	death_year smallint null
+);
+
+create table media (
+	id serial constraint pk_media primary key,
+	title varchar(500) null,
+	original_title varchar(500) null,
+	media_type varchar(15) null,
+	release_year smallint null,
+	end_year smallint null,
+	duration_mn int null
+);
+
+create table have_genre(
+	media_id int not null,
+	genre varchar(20) not null
+);
+
+create table profession (
+	id serial constraint pk_profession primary key,
+	name varchar(150) not null constraint prof_unique unique
+);
+
+create table have_profession(
+	person_id int not null,
+	prof_id int not null,
+	constraint pk_has_prof primary key(person_id, prof_id)
+);
+
+create table known_for(
+	person_id int not null,
+	media_id int not null,
+	constraint pk_known_for primary key(person_id, media_id)
+);
+
+create table direct(
+	media_id int not null,
+	director_id int not null,
+	constraint pk_direct primary key(media_id, director_id)
+);
+
+create table play(
+	media_id int not null,
+	actor_id int not null,
+	ordering int null,
+	character varchar(500)
+);
+
+create table have_episode(
+	series_id int not null,
+	episode_id int not null,
+	season_number int,
+	episode_number int
+);
+
+create table aka(
+	id serial constraint pk_aka primary key,
+	media_id int not null,
+	ordering int,
+	title varchar(900) not null,
+	region varchar(4),
+	language varchar(4),
+	aka_types varchar(100),
+	aka_attributes varchar(100),
+	is_original boolean
+);
